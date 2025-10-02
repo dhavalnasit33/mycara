b8b41836c5852cdaaed4f0e3a22130ddd0e6cada
dhavalnasit33 <dhavalnasit33@gmail.com>
2025-10-02 11:43:09 +0530

    Your commit message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,55 +1,46 @@
+
 // D:\mycara\frontend\src\App.jsx
-
 import React from "react";
-
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom"; 
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import './App.css';
 import './index.css';
-
-
-import Home from "./pages/Home"; 
+import Home from "./pages/Home";
 import Shop from "./pages/Shop";
 import Collections from "./pages/Collections";
 import Blogs from "./pages/Blogs";
 import Features from "./pages/Features";
 import More from "./pages/More";
-
-
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import LoginForm from "./pages/Login";
 import RegistrationForm from "./pages/RegistrationForm";
-
 function App() {
-    
-
   const RouterWrapper = () => {
-    
     const location = useLocation();
-    
-    
     const isShopPage = location.pathname === "/shop";
-
     return (
-      <Router>
-       
+      <>
         <Header hideOnMobileShopPage={isShopPage} />
-        
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Home" element={<Home />} />
-          <Route path="/shop" element={<Shop />} /> 
+          <Route path="/shop" element={<Shop />} />
           <Route path="/collections" element={<Collections />} />
           <Route path="/blogs" element={<Blogs />} />
           <Route path="/features" element={<Features />} />
           <Route path="/more" element={<More />} />
-          <Route path="/Login" element={<LoginForm />} />
-          <Route path="/Register" element={<RegistrationForm />} />
-      </Routes>
-      <Footer />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/register" element={<RegistrationForm />} />
+        </Routes>
+        <Footer />
+      </>
+    );
+  };
+  return (
+    <Router>
+      <RouterWrapper />
     </Router>
   );
-}
 }
 
 export default App;
